49962397fdd3d17112354dc9d9c8c4a2fa89678b
Lucas Melo <luucasmell@gmail.com>
2019-07-27 17:12:19 -0300

    Adicionar funcao para inicar e comecar cronometro

---
+++ script.js
@@ -0,0 +1,29 @@
+let comecar_parar = document.querySelector("#comecar_parar");
+let cronometro = document.querySelector("#cronometro");
+let iniciado = false, hora_inicio, hora_atual, init_cronometro, tempo_passado;
+
+
+function controlar(){
+    if(!iniciado){
+        iniciado = true;
+        comecar_parar.innerHTML = "Parar";
+
+        //Começar o cronômetro
+        hora_inicio = new Date().getTime();
+
+        init_cronometro = setInterval(() => {
+
+            hora_atual = new Date().getTime();
+            tempo_passado = hora_atual - hora_inicio;
+
+            cronometro.innerHTML= tempo_passado;
+        }, 10);
+
+    } else{
+        clearInterval(init_cronometro);
+        iniciado = false;
+        comecar_parar.innerHTML = "Começar"
+    }
+}
+
+comecar_parar.onclick = controlar;
